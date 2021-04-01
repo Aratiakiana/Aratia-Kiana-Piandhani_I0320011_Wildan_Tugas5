142e1bd3f6165ee2f79e3574e0bd16fbd52f78b9
Aratia Kiana Piandhani <aratiakiana.p@gmail.com>
2021-04-01 20:50:09 +0700

    Pengerjaan soal

---
+++ I0320011_Soal1_Tugas5.py
@@ -0,0 +1,8 @@
+NamaPengunjung = input("Masukkan Nama Pengunjung :")
+JenisKelamin = input("Masukkan Jenis Kelamin (L/P) :")
+
+# Memasukkan nama dan jenis kelamin
+if JenisKelamin == "L" :
+    print("Selamat datang, Tuan",NamaPengunjung,"!")
+elif JenisKelamin == "P" :
+    print("Selamat datang, Nyonya",NamaPengunjung,"!")
